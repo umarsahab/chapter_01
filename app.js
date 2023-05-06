1b2b459f6564e1a639ad4ef745b25b363918d2f7
umarsahab <121605324+umarsahab@users.noreply.github.com>
2023-05-06 14:22:30 +0500

    submit

---
+++ app.js
@@ -0,0 +1,34 @@
+
+
+ // Chapter1
+ var userName = prompt("Inter Your Name")
+ alert("Hello Sir  "+ userName + "  how are you?")
+
+// Question 2 
+
+alert("Error! Please enter Valid password")
+
+// Question 3
+alert("Welcome to js land..." + "\n" + "happy coding")
+
+// Question 4
+alert("Welcome to js land...")
+alert("happy coding")
+
+ // Question 5
+ var message = "Hello.. I can run js through my web browsers console";
+ alert(message)
+ console.log(message);
+
+// Question 6
+// Question 7
+//  Practice placement of <script></script> element in 
+//  following sections of your project in exercise 6:
+//  Head
+alert("Head--> it will  work ") 
+//  Body (before your page’s HTML)
+alert("Body (before your page’s HTML)--> it will not work")  
+//  c. Body (inside your page’s HTML)
+alert("Body (inside your page’s HTML)--> it will  work ")
+//  d. Body (after your page’s HTML)
+alert(" Body (after your page’s HTML)--> it will not work")
